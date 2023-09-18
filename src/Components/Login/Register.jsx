d83cfef777121c8a71fc8f5484eebd81f4648b53
MonzurAlam <you@example.com>
2023-09-18 14:28:18 +0600

    a comment jsut

--- src/Components/Login/Register.jsx
+++ src/Components/Login/Register.jsx
@@ -182,7 +182,7 @@ const Register = () => {
           <div className="divider w-96 mx-auto font-bold">OR</div>
           <div className="grid items-center justify-center">
             <button
-              onClick={() => signInWithGoogle()}
+              //   onClick={() => signInWithGoogle()}
               className="btn btn-outline  bg-emerald-500 hover:bg-emerald-700 text-white w-80 font-bold "
             >
               <FcGoogle className="w-10 h-6"></FcGoogle> Google Sign In
